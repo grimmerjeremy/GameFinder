6a8279e9b57ca1f530ff05340ff93bb1164d4bb8
grimmerjeremy <79933447+grimmerjeremy@users.noreply.github.com>
2021-04-15 09:25:14 -0400

    Ticket 5

--- GameFinder.Data/GameConsole.cs
+++ GameFinder.Data/GameConsole.cs
@@ -13,5 +13,6 @@ namespace GameFinder.Data
         public int ConsoleId { get; set; }
         [Required]
         public string ConsoleName { get; set; }
+        public virtual List<Game> GamesOnConsole { get; set; } = new List<Game>();
     }
 }
